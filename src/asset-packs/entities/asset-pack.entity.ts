a04bd52f52e0e9a043f08b87713df5e8c355d403
beland.dev <>
2022-04-01 17:03:55 +0700

    update asset model

--- src/asset-packs/entities/asset-pack.entity.ts
+++ src/asset-packs/entities/asset-pack.entity.ts
@@ -1,1 +1,40 @@
-export class AssetPack {}
+import { ARRAY } from 'sequelize';
+import { JSONB } from 'sequelize';
+import { STRING } from 'sequelize';
+import {
+  AllowNull,
+  Column,
+  CreatedAt,
+  Index,
+  IsUUID,
+  Model,
+  PrimaryKey,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+@Table({ tableName: 'asset_packs' })
+export class AssetPack extends Model {
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  id: string;
+
+  @Index
+  @AllowNull(false)
+  @Column
+  eth_address: string;
+
+  @AllowNull(false)
+  @Column
+  title: string;
+
+  @Column
+  thumbnail: boolean;
+
+  @CreatedAt
+  created_at: Date;
+
+  @UpdatedAt
+  updated_at: Date;
+}
--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,1 +1,72 @@
-export class Asset {}
+import { ARRAY } from 'sequelize';
+import { JSONB } from 'sequelize';
+import { STRING } from 'sequelize';
+import {
+  AllowNull,
+  Column,
+  CreatedAt,
+  Default,
+  Index,
+  IsUUID,
+  Model,
+  PrimaryKey,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+@Table({ tableName: 'assets' })
+export class Asset extends Model {
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  id: string;
+
+  @AllowNull(false)
+  @Column
+  name: string;
+
+  @Index
+  @IsUUID(4)
+  @AllowNull(false)
+  @Column
+  pack_id: string;
+
+  @Index
+  @AllowNull(false)
+  @Column
+  eth_address: string;
+
+  @AllowNull(false)
+  @Column
+  model: string;
+
+  @Column
+  script: string;
+
+  @Column
+  thumbnail: boolean;
+
+  @Column(ARRAY(STRING))
+  tags: Array<string>;
+
+  @Column
+  category: string;
+
+  @Column(JSONB)
+  metrics: Record<string, string>;
+
+  @Column(JSONB)
+  contents: Record<string, string>;
+
+  @Column(JSONB)
+  parameters: any;
+
+  @Column(JSONB)
+  actions: any;
+
+  @CreatedAt
+  created_at: Date;
+
+  @UpdatedAt
+  updated_at: Date;
+}
